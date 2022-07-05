ad348acc0bd568f1416a5f8e4d043786f2ae7674
Fabio <fs.embarcados@gmail.com>
2022-07-04 21:57:17 -0300

    Ajustando configuração para WROOM

--- boards/esp32/FRANZININHO_ESP32_S2_WROOM/mpconfigboard.h
+++ boards/esp32/FRANZININHO_ESP32_S2_WROOM/mpconfigboard.h
@@ -1,8 +1,12 @@
-#define MICROPY_HW_BOARD_NAME               "ESP32S2 module"
-#define MICROPY_HW_MCU_NAME                 "ESP32S2"
+#define MICROPY_HW_BOARD_NAME           "Franzininho WiFi"
+#define MICROPY_HW_MCU_NAME             "ESP32-S2"
 
-#define MICROPY_PY_BLUETOOTH                (0)
-#define MICROPY_HW_ENABLE_SDCARD            (0)
+#define MICROPY_PY_BLUETOOTH            (0)
+#define MICROPY_HW_ENABLE_SDCARD        (0)
 
-// Enable UART REPL for modules that have an external USB-UART and don't use native USB.
-#define MICROPY_HW_ENABLE_UART_REPL         (1)
+#define MICROPY_HW_I2C0_SCL             (9)
+#define MICROPY_HW_I2C0_SDA             (8)
+
+#define MICROPY_HW_SPI1_MOSI            (35)
+#define MICROPY_HW_SPI1_MISO            (36)
+#define MICROPY_HW_SPI1_SCK             (37)
