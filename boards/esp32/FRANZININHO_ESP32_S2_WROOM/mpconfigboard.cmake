ba57c6f1702a00d122f12e81c899e154297fb044
Emerson Henning <emerson@henning.com.br>
2022-07-07 15:14:35 -0300

    Alterado o build.sh para gerar os binários para as duas franzininhos e corrigido uma configuração da WROOM que estava apontando para uma pasta invalida

--- boards/esp32/FRANZININHO_ESP32_S2_WROOM/mpconfigboard.cmake
+++ boards/esp32/FRANZININHO_ESP32_S2_WROOM/mpconfigboard.cmake
@@ -3,7 +3,7 @@ set(IDF_TARGET esp32s2)
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.usb
-    boards/FRANZININHO_WIFI/sdkconfig.board
+    boards/FRANZININHO_ESP32_S2_WROOM/sdkconfig.board
 )
 
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 rm -Rf micropython/ports/esp32/boards/FRANZININHO_ESP32_S2_WROOM
-mkdir micropython/ports/esp32/boards/FRANZININHO_ESP32_S2_WROOM 
-mkdir build
-cp boards/esp32/FRANZININHO_ESP32_S2_WROOM/* micropython/ports/esp32/boards/FRANZININHO_ESP32_S2_WROOM/.
+rm -Rf micropython/ports/esp32/boards/FRANZININHO_ESP32_S2_WROOM
+mkdir -p build/FRANZININHO_ESP32_S2_WROOM
+mkdir -p build/FRANZININHO_WIFI_WROVER
+cp -R boards/esp32/* micropython/ports/esp32/boards/
 cd micropython/ports/esp32
-idf.py -D MICROPY_BOARD=FRANZININHO_ESP32_S2_WROOM -B ../../../build  fullclean
-idf.py -D MICROPY_BOARD=FRANZININHO_ESP32_S2_WROOM -B ../../../build  build
+idf.py -D MICROPY_BOARD=FRANZININHO_ESP32_S2_WROOM -B ../../../build/FRANZININHO_ESP32_S2_WROOM  fullclean
+idf.py -D MICROPY_BOARD=FRANZININHO_ESP32_S2_WROOM -B ../../../build/FRANZININHO_ESP32_S2_WROOM  build
+idf.py -D MICROPY_BOARD=FRANZININHO_WIFI_WROVER -B ../../../build/FRANZININHO_WIFI_WROVER  fullclean
+idf.py -D MICROPY_BOARD=FRANZININHO_WIFI_WROVER -B ../../../build/FRANZININHO_WIFI_WROVER  build
